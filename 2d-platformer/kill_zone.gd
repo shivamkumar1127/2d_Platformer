dc94ea3e3048e0089c937b64de2d9ebdee9c5215
shivamkumar1127 <officialshivamkumar07@gmail.com>
2026-07-05 16:44:05 +0530

    Trying Death Message

--- 2d-platformer/kill_zone.gd
+++ 2d-platformer/kill_zone.gd
@@ -1,12 +1,20 @@
 extends Area2D
-@onready var timer: Timer = $Timer
 
+@onready var timer: Timer = $Timer
+# Reference the UI label you just created
+@onready var death_message: Label = get_node("../CanvasLayer/DeathMessage")
 
 func _on_body_entered(body: Node2D) -> void:
 	print("you died")
-	timer.start()
 	
-
+	# Show the UI label
+	death_message.visible = true
+	
+	# Optional: Stop the player from moving so they don't slide 
+	# while the death message is showing
+	body.queue_free() 
+	
+	timer.start()
 
 func _on_timer_timeout() -> void:
 	get_tree().reload_current_scene()
