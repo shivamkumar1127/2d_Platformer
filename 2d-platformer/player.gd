6a20c8d1d4196df5d001091b3ce4336aa6bd2281
shivamkumar1127 <officialshivamkumar07@gmail.com>
2026-07-05 11:03:26 +0530

    Added Animations
    Added all the basic animations and the flipping of character in the respective directions...

--- 2d-platformer/player.gd
+++ 2d-platformer/player.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
-
 const SPEED = 150.0
 const JUMP_VELOCITY = -280.0
 
+# Reference to your AnimatedSprite2D
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -15,8 +16,24 @@ func _physics_process(delta: float) -> void:
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
+	
+	# --- ANIMATION & FLIPPING LOGIC START ---
+	if direction != 0:
+		animated_sprite.flip_h = (direction < 0)
+		
+	if not is_on_floor():
+		if velocity.y < 0:
+			animated_sprite.play("Jump")
+		else:
+			animated_sprite.play("Fall")
+	else:
+		if direction != 0:
+			animated_sprite.play("Run")
+		else:
+			animated_sprite.play("Idle")
+	# --- ANIMATION & FLIPPING LOGIC END ---
+
 	if direction:
 		velocity.x = direction * SPEED
 	else:
