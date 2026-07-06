3dadb12e95bbaf972e04329f2f82cfed32c0f645
shivamkumar1127 <officialshivamkumar07@gmail.com>
2026-07-06 17:17:33 +0530

    Fixed some bugs
    added coin count reset after winning the game

--- 2d-platformer/win_screen.gd
+++ 2d-platformer/win_screen.gd
@@ -1,6 +1,9 @@
 extends Control
 
 func _on_menu_button_pressed() -> void:
+	# Reset the score to 0 before transitioning back to the menu
+	GameManager.score = 0
+	
 	# This triggers the change more smoothly
 	get_tree().call_deferred("change_scene_to_file", "res://main_menu.tscn")
 
