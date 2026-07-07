64bec5eb1e9fd20320e8980f0a20f922588106fd
machaaaryan18-hub <aestheticaura18@gmail.com>
2026-07-07 22:48:45 +0530

    back

--- 2d-platformer/score_label.gd
+++ 2d-platformer/score_label.gd
@@ -3,3 +3,7 @@ extends Label
 func _process(_delta: float) -> void:
 	text = "Coins: " + str(GameManager.score)
 	# The print statement that was causing the spam is removed
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://main_menu.tscn")
